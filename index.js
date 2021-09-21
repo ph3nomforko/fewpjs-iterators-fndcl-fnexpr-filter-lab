ec29992590821547a07b6f061c7258d29809cf63
Codi Kraus <codi.kraus@gmail.com>
2021-09-21 15:53:10 -0500

    commit tree b5e2d1029ca6185a543701846f52d2db3feadeb5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter( driver =>
+        driver.toLowerCase() ===     string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( driver => 
+        driver.toLowerCase().indexOf(string.toLowerCase()) === 0 )
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( driver =>
+        driver.name === string)
+}
